5d31f0bc99981b4423087633167401706b24e129
Davis Brayall <djbray79@yahoo.com>
2020-11-13 22:10:08 -0500

    scraped more stats and adding another function

--- DnDCharacter.py
+++ DnDCharacter.py
@@ -5,53 +5,21 @@ import json
 # Creating classes for the diffrent types of character info
 class CharInfo:
     character_name = []
-    character_lvl = []
+    character_level = []
     character_class = []
     character_race = []
     character_gender = []
     character_age = []
+    speed = []
 
 
-class StrInfo: 
+class Stats: 
     strength = []
-    #athletics = []
-
-
-class DexInfo:
     dextarity = []
-    #acrobatics = []
-    #sleight_of_hand = []
-    #stealth = []
-
-
-class ConInfo:
     constitution = []
-
-
-class IntInfo:
     intelagence = []
-    #arcana = []
-    #history = []
-    #investigation = []
-    #nature = []
-    #religion = []
-
-
-class WisInfo:
     wisdom = []
-    #animal_handling = []
-    #insight = []
-    #medicine = []
-    #perception = []
-    #survival = []
-
-
-class ChaInfo:
     charisma = []
-    #deception = []
-    #intimidation = []
-    #performance = []
-    #persuasion = []
 
 
 class HealthInfo:
@@ -60,10 +28,25 @@ class HealthInfo:
 
 
 class OtherInfo:
-    #armor_class = []
-    #initiative = []
+    armor_class = []
+    initiative = []
     #proficiency = []
-    speed = []
+    
+
+def proficiencyBonus():
+    if character_level == 1 or 2 or 3 or 4:
+            proficiency = 2
+    elif character_level == 5 or 6 or 7 or 8:
+            proficiency = 3
+    elif character_level == 9 or 10 or 11 or 12:
+            proficiency = 4
+    elif character_level == 13 or 14 or 15 or 16:
+            proficiency = 5
+    elif character_level == 17 or 18 or 19 or 20:
+            proficiency = 6
+    else:
+        proficiency = 7
+    return proficiency
 
 
 def main():
@@ -81,29 +64,44 @@ def main():
     main_class = classes_refine['definition']
     modifiers = sheet['modifiers']
 
+
     base_strength = stats[0]['value']
-    base_dextarity = stats[1]['value']
+    base_dexterity = stats[1]['value']
     base_constitution = stats[2]['value']
     base_intelagence = stats[3]['value']
     base_wisdom = stats[4]['value']
     base_charisma = stats[5]['value']
     base_hitpoints = sheet['baseHitPoints']
     base_speed = race['weightSpeeds']['normal']['walk']
+    base_armor_class = [10]
     removed_hitpoints = sheet['removedHitPoints']
 
-    #race_modify_strength = []
-    race_modify_dextarity = modifiers['race'][6]['value']
-    #race_modify_constitution = []
+    
+    race_modify_dexterity = modifiers['race'][6]['value']
     race_modify_intelagence = modifiers['race'][5]['value']
-    #race_modify_wisdom = []
     race_modify_charisma = modifiers['race'][4]['value']
 
-    #class_modify_strength = []
-    #class_modify_dextarity = []
-    #class_modify_constitution = []
-    #class_modify_intelagence = []
+    
     class_modify_wisdom = modifiers['class'][0]['value']
     class_modify_charisma = modifiers['class'][1]['value']
+    unarmored_class_modifier = modifiers['class'][14]['value']
+
+
+    strength = base_strength
+    dexterity = base_dexterity + race_modify_dexterity
+    constitution = base_constitution 
+    intelagence = base_intelagence + race_modify_intelagence
+    wisdom = base_wisdom + class_modify_wisdom
+    charisma = base_charisma + race_modify_charisma + class_modify_charisma
+
+
+    modifier_strength = (strength - 10) // 2 
+    modifier_dexterity = (dexterity - 10) // 2
+    modifier_constitution = (constitution - 10) // 2
+    modifier_intelangence = (intelagence - 10) // 2
+    modifier_wisdom = (wisdom - 10) // 2
+    modifier_charisma = (charisma - 10) // 2
+
 
     character_name = sheet['name']
     character_level = classes_refine['level']
@@ -113,8 +111,20 @@ def main():
     character_age = sheet['age']
     max_hitpoints = base_hitpoints + character_level
     current_hitpoints = max_hitpoints - removed_hitpoints
+    initiative = modifier_dexterity
+    proficiency = proficiencyBonus()
 
-    print(character_name, current_hitpoints)
+
+    #code that has not yet been added
+    #race_modify_strength = []
+    #race_modify_constitution = []
+    #race_modify_wisdom = []
+    #class_modify_strength = []
+    #class_modify_dextarity = []
+    #class_modify_constitution = []
+    #class_modify_intelagence = []
+
+    print(character_name, proficiency)
 
 if __name__ == "__main__":
     main()
