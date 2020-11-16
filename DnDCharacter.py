d62bf62af0cbfea95dffec85e6df6cf3ca0cc807
Davis Brayall <djbray79@yahoo.com>
2020-11-15 20:36:22 -0500

    added some functions and cleaned code up a bit

--- DnDCharacter.py
+++ DnDCharacter.py
@@ -1,5 +1,22 @@
 import requests
 import json
+import math
+
+
+#import D&Dbeyond character json API and parse it
+url = requests.get (
+        "https://character-service.dndbeyond.com/character/v3/character/6917300"
+        )
+
+page_dict = json.loads(url.text)
+
+sheet = page_dict['data']
+stats = sheet['stats']
+race = sheet['race']
+classes = sheet['classes']
+classes_refine = classes[0]
+main_class = classes_refine['definition']
+modifiers = sheet['modifiers']
 
 
 # Creating classes for the diffrent types of character info
@@ -17,7 +34,7 @@ class Stats:
     strength = []
     dextarity = []
     constitution = []
-    intelagence = []
+    intelligence = []
     wisdom = []
     charisma = []
 
@@ -30,77 +47,74 @@ class HealthInfo:
 class OtherInfo:
     armor_class = []
     initiative = []
-    #proficiency = []
+    proficiency = []
+
+def proficiencyBonus(character_level):
+    bonus = math.ceil(1 + (0.25 * (character_level)))
+    return bonus
+
+def totalStats(base, race, clas):
+    total = base + race + clas
+    return total
+
+def getModifier(ability_score):
+    modifier = (ability_score - 10) //2
+    return modifier
+
+def base_stats(base):
+        base_score = stats[base]['value']
+        return base_score
+
+def race_modify(mod):
+    race_modify = modifiers['race'][mod]['value']
+    return race_modify
+
+def class_modify(mod):
+    class_modify = modifiers['class'][mod]['value']
+    return class_modify
 
 
+# this main function pulls down the character sheet and pulls out the relivant data
 def main():
-    url = requests.get (
-        "https://character-service.dndbeyond.com/character/v3/character/6917300"
-        )
 
-    page_dict = json.loads(url.text)
-
-    sheet = page_dict['data']
-    stats = sheet['stats']
-    race = sheet['race']
-    classes = sheet['classes']
-    classes_refine = classes[0]
-    main_class = classes_refine['definition']
-    modifiers = sheet['modifiers']
+    base_strength = base_stats(0)
+    base_dexterity = base_stats(1)
+    base_constitution = base_stats(2)
+    base_intelligence = base_stats(3)
+    base_wisdom = base_stats(4)
+    base_charisma = base_stats(5)
 
 
-    base_strength = stats[0]['value']
-    base_dexterity = stats[1]['value']
-    base_constitution = stats[2]['value']
-    base_intelagence = stats[3]['value']
-    base_wisdom = stats[4]['value']
-    base_charisma = stats[5]['value']
     base_hitpoints = sheet['baseHitPoints']
+    removed_hitpoints = sheet['removedHitPoints']
     base_speed = race['weightSpeeds']['normal']['walk']
     base_armor_class = [10]
-    removed_hitpoints = sheet['removedHitPoints']
 
     
-    race_modify_dexterity = modifiers['race'][6]['value']
-    race_modify_intelagence = modifiers['race'][5]['value']
-    race_modify_charisma = modifiers['race'][4]['value']
+    race_modify_dexterity = race_modify(6)
+    race_modify_intelligence = race_modify(5)
+    race_modify_charisma = race_modify(4)
 
     
-    class_modify_wisdom = modifiers['class'][0]['value']
-    class_modify_charisma = modifiers['class'][1]['value']
-    unarmored_class_modifier = modifiers['class'][14]['value']
+    class_modify_wisdom = class_modify(0)
+    class_modify_charisma = class_modify(1)
+    unarmored_class_modifier = class_modify(14)
 
 
-    strength = base_strength
-    dexterity = base_dexterity + race_modify_dexterity
-    constitution = base_constitution 
-    intelagence = base_intelagence + race_modify_intelagence
-    wisdom = base_wisdom + class_modify_wisdom
-    charisma = base_charisma + race_modify_charisma + class_modify_charisma
+    strength = totalStats(base_strength, 0, 0)
+    dexterity = totalStats(base_dexterity, race_modify_dexterity, 0)
+    constitution = totalStats(base_constitution, 0, 0) 
+    intelligence = totalStats(base_intelligence, race_modify_intelligence, 0)
+    wisdom = totalStats(base_wisdom, 0, class_modify_wisdom)
+    charisma = totalStats(base_charisma, race_modify_charisma, class_modify_charisma)
+        
 
-
-    modifier_strength = (strength - 10) // 2 
-    modifier_dexterity = (dexterity - 10) // 2
-    modifier_constitution = (constitution - 10) // 2
-    modifier_intelangence = (intelagence - 10) // 2
-    modifier_wisdom = (wisdom - 10) // 2
-    modifier_charisma = (charisma - 10) // 2
-    
-
-    def proficiencyBonus():
-        if character_level == 1 or 2 or 3 or 4:
-                proficiency = 2
-        elif character_level == 5 or 6 or 7 or 8:
-                proficiency = 3
-        elif character_level == 9 or 10 or 11 or 12:
-                proficiency = 4
-        elif character_level == 13 or 14 or 15 or 16:
-                proficiency = 5
-        elif character_level == 17 or 18 or 19 or 20:
-                proficiency = 6
-        else:
-            proficiency = 7
-        return proficiency
+    modifier_strength = getModifier(strength) 
+    modifier_dexterity = getModifier(dexterity)
+    modifier_constitution = getModifier(constitution)
+    modifier_intelligence = getModifier(intelligence)
+    modifier_wisdom = getModifier(wisdom)
+    modifier_charisma = getModifier(charisma)
 
 
     character_name = sheet['name']
@@ -112,7 +126,7 @@ def main():
     max_hitpoints = base_hitpoints + character_level
     current_hitpoints = max_hitpoints - removed_hitpoints
     initiative = modifier_dexterity
-    proficiency = proficiencyBonus()
+    proficiency = proficiencyBonus(character_level)
 
 
     #code that has not yet been added
@@ -122,9 +136,9 @@ def main():
     #class_modify_strength = []
     #class_modify_dextarity = []
     #class_modify_constitution = []
-    #class_modify_intelagence = []
+    #class_modify_intelligence = []
 
-    print(character_name, character_level, proficiency)
+    print(character_name, class_modify_wisdom, race_modify_dexterity)
 
 if __name__ == "__main__":
     main()
